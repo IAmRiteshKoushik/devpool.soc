2d974a83224b4144486ddd808474d3d6ef43ad3e
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-12-31 07:12:49 +0530

    Launching consumers and reading data from Redis

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"sync"
+
+	"github.com/IAmRiteshKoushik/devpool/cmd"
+	"github.com/IAmRiteshKoushik/devpool/services"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	if err := cmd.InitValkey(); err != nil {
+		log.Fatalf("Failed to initialize Valkey: %v", err)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(4)
+
+	go func() {
+		defer wg.Done()
+		services.ConsumeIssueStream()
+	}()
+
+	go func() {
+		defer wg.Done()
+		services.ConsumeBountyStream()
+	}()
+
+	go func() {
+		defer wg.Done()
+		services.ConsumeSolutionStream()
+	}()
+
+	go func() {
+		defer wg.Done()
+		services.ConsumeAchievementStream()
+	}()
+
+	wg.Wait()
 }
